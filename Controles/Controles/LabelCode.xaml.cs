9e042e737503aa00c03b8cd4171e6a2169a3e962
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2021-04-05 20:17:44 -0500

    First commit

---
+++ Controles/Controles/LabelCode.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelCode : ContentPage
+    {
+        public LabelCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var underlineLabel = new Label
+            {
+                Text = "This is underlined text.",
+                TextDecorations = TextDecorations.Underline
+            };
+            var strickethroughLabel = new Label
+            {
+                Text = "This is text with strickethrough.",
+                TextDecorations = TextDecorations.Strikethrough
+            };
+            var bothLabel = new Label
+            {
+                Text = "This is underlined text with strickethrough.",
+                TextDecorations = TextDecorations.Underline | TextDecorations.Strikethrough
+            };
+
+            stack.Children.Add(underlineLabel);
+            stack.Children.Add(strickethroughLabel);
+            stack.Children.Add(bothLabel);
+            Content = stack;
+        }
+    }
+}
